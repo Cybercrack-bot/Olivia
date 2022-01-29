9bc84648cd31811fd5bd525447b199a026dc4595
HackerCyberCrack <73816379+Cybercrack-bot@users.noreply.github.com>
2022-01-29 13:51:57 +0530

    Delete Core directory

--- Core/functions/search_youtube.py
+++
@@ -1,5 +0,0 @@
-def search_youtube(query):
-    import webbrowser
-    from text_to_speech import speak
-    speak("Searching Youtube for {}, please give me a second".format(query))
-    webbrowser.open("https://www.youtube.com/results?search_query=" + query.replace(" ", "+"), save=False)
