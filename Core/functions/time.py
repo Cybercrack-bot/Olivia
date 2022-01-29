a1ce765e0ae3911ef7aed2f2aa478c68ebdb6d74
HackerCyberCrack <73816379+Cybercrack-bot@users.noreply.github.com>
2022-01-29 11:43:57 +0530

    Delete Core directory

--- Core/functions/time.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from text_to_speech import speak
-
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-speak(current_time)
