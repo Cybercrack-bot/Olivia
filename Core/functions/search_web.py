ed9c8e6cfe50b47d685c17a119d9b63ad895e552
HackerCyberCrack <73816379+Cybercrack-bot@users.noreply.github.com>
2022-02-02 11:48:19 +0530

    Delete Core directory

--- Core/functions/search_web.py
+++
@@ -1,7 +0,0 @@
-def search_google(query):
-    import webbrowser
-    from googlesearch import search
-    from text_to_speech import speak
-    webbrowser.open("https://www.google.com/search?q={}".format(query))
-
-    
